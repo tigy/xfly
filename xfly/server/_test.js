e7d4f6ee7c36587694dee83c83b1a9aed96d3595
unknown <fengyuxiang@11A6P-3A8A710A.lefeng.max>
2014-04-21 19:30:54 +0800

    项目环境部署

--- xfly/server/_test.js
+++ xfly/server/_test.js
@@ -1,2 +1,14 @@
 var IO=require("utilskit/io");
-IO.deleteFile("D:/aa.txt");
+//IO.deleteFile("D:/aa.txt");
+
+var HttpUtility = require('./webserver/httputility');
+var sss = "a=b&t=5";
+var url = "http://www.baidu.com/a.php?a=b&t=6";
+var val = HttpUtility.parseQueryString(sss);
+console.log(val["t"]);
+
+
+
+//console.log(s1);
+//var s=new HttpValueCollection();
+//s.fillFromString(url);
